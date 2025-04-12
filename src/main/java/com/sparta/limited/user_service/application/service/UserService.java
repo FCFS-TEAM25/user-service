af3a89c7cc23d8f4fce3fde8bd2df22b904329aa
Imnotcoderdude <topsolo7@naver.com>
2025-04-12 21:51:20 +0900

    ✨ feat [#1] : 사용자 생성을 위한 service 클래스 작성 - 작성한 Mapper 클래스를 사용하여 사용자 생성, DTO 변환 책임 분리

---
+++ src/main/java/com/sparta/limited/user_service/application/service/UserService.java
@@ -0,0 +1,25 @@
+package com.sparta.limited.user_service.application.service;
+
+import com.sparta.limited.user_service.application.mapper.UserMapper;
+import com.sparta.limited.user_service.domain.model.User;
+import com.sparta.limited.user_service.domain.repository.UserRepository;
+import com.sparta.limited.user_service.infrastructure.dto.request.UserCreateFromAuthRequest;
+import com.sparta.limited.user_service.infrastructure.dto.response.UserCreateFromAuthResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Transactional
+    public UserCreateFromAuthResponse createUser(UserCreateFromAuthRequest request) {
+        User user = userMapper.toEntity(request);
+        userRepository.save(user);
+        return userMapper.toResponse(user);
+    }
+}
