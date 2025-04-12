febf69cbb27cf6c2ec95140e81c9c40118dc7d6f
Imnotcoderdude <topsolo7@naver.com>
2025-04-12 22:53:52 +0900

    ✨ feat [#1] : Enum 클래스에서 설명 필드 추가 - 해당 Enum이 무슨 의미로 사용되는지 명시적으로 표현하기 위한 설명 필드 추가

--- src/main/java/com/sparta/limited/user_service/domain/model/Gender.java
+++ src/main/java/com/sparta/limited/user_service/domain/model/Gender.java
@@ -1,11 +1,19 @@
 package com.sparta.limited.user_service.domain.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Gender {
 
-    FEMALE,
-    MALE;
+    FEMALE("여성"),
+    MALE("남성");
+
+    private final String description;
 
     public static Gender from(String gender) {
         return Gender.valueOf(gender);
     }
+
 }
--- src/main/java/com/sparta/limited/user_service/domain/model/UserRole.java
+++ src/main/java/com/sparta/limited/user_service/domain/model/UserRole.java
@@ -1,9 +1,16 @@
 package com.sparta.limited.user_service.domain.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
 
-    ROLE_ADMIN,
-    ROLE_USER;
+    ROLE_ADMIN("관리자"),
+    ROLE_USER("일반 사용자");
+
+    private final String description;
 
     public static UserRole from(String role) {
         return UserRole.valueOf(role);
