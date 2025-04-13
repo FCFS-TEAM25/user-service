e2ba9dc0ce5df8db32a1a8546a7b4bc8a6ee68f9
Imnotcoderdude <topsolo7@naver.com>
2025-04-13 19:16:27 +0900

    ✨ feat [#5] : 서비스 클래스 username 기반 사용자 조회 메서드 추가  - @Transactional(readOnly = true) 어노테이션을 활용하여 조회 기능임을 명시적으로 작성

--- src/main/java/com/sparta/limited/user_service/application/service/UserService.java
+++ src/main/java/com/sparta/limited/user_service/application/service/UserService.java
@@ -7,24 +7,31 @@ import com.sparta.limited.user_service.domain.model.User;
 import com.sparta.limited.user_service.domain.repository.UserRepository;
 import com.sparta.limited.user_service.infrastructure.dto.request.UserCreateFromAuthRequest;
 import com.sparta.limited.user_service.infrastructure.dto.response.UserCreateFromAuthResponse;
-import jakarta.transaction.Transactional;
+import com.sparta.limited.user_service.infrastructure.dto.response.UserSearchUsernameResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
 
     @Transactional
     public UserCreateFromAuthResponse createUser(UserCreateFromAuthRequest request) {
         if (userRepository.existsByUsername(request.getUsername())) {
             throw new BusinessException(ErrorCode.DUPLICATE_RESOURCE);
         }
-        User user = userMapper.toEntity(request);
+        User user = UserMapper.toEntity(request);
         userRepository.save(user);
-        return userMapper.toResponse(user);
+        return UserMapper.toResponse(user);
     }
+
+    @Transactional(readOnly = true)
+    public UserSearchUsernameResponse searchUserFromUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        return UserMapper.toSearchUsernameResponse(user);
+    }
+
 }
